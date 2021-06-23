32105ce911bbc29610cea177175fda9bad15bdc7
Michael <michael.baker279@yahoo.com>
2021-06-23 16:13:20 -0500

    slight tweaks doesnt make it any better, just there for clarity

--- ModelCreation.py
+++ ModelCreation.py
@@ -15,9 +15,9 @@ dense = layers.Dense(710, activation="relu")
 x = dense(inputs)
 x = layers.Dense(420, activation="sigmoid")(x)
 x = layers.Dense(710, activation="softmax")(x)
-x = layers.Dense(69, activation="softplus")(x)
-x = layers.Dense(6, activation="softsign")(x)
-x = layers.Dense(9, activation="tanh")(x)
+x = layers.Dense(69, activation="tanh")(x)
+x = layers.Dense(6, activation="softplus")(x)
+x = layers.Dense(9, activation="selu")(x)
 outputs = layers.Dense(1)(x)
 
 #make sure to insert the file name that we are pulling data from
@@ -36,7 +36,7 @@ count=0
 random.shuffle(dataPoints)
 for row in dataPoints:
     #change this number to properly spit your data into training and testing data, a 80/20 or 85/15 split is normal
-    if count<2500:
+    if count<240:
         x_train.append(row[0:9])
         y_train.append(row[10])
     else:
@@ -50,8 +50,8 @@ y_train=np.asarray(y_train).astype(float)
 y_test=np.asarray(y_test).astype(float)
 
 model = keras.Model(inputs=inputs, outputs=outputs, name="Forex_NN")
-#fill in optimizer and any other arguments you wish
-model.compile(optimizer='', metrics=['accuracy'])
+#fill in  with additional arguments like optimizer accuracy and loss
+model.compile()
 #feel free to try any other arguments with fit as well, some basic ones to play with are batch_size and epochs and maybe validation_split
 history = model.fit(x_train, y_train)
 
